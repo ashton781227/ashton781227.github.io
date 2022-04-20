73085fccc7e4ca02b39921674f32a70a27a60f2d
Ashton Pieterse <ashtonpieterse.ap@gmail.com>
2022-04-20 16:30:33 +0200

    Added to index

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,4 @@
 const currentDate = new Date();
 document.querySelector('#year').textContent = currentDate.getFullYear();
+let lastUpdate = document.lastModified;
+document.querySelector('#last_update').textContent = lastUpdate;
