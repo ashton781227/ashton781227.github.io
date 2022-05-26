085c4376b38289f8de8b2703423eb8286fa018be
Ashton Pieterse <ashtonpieterse.ap@gmail.com>
2022-05-26 12:28:49 +0200

    added lesson 11 folder and files

---
+++ lesson-11/js/windchill.js
@@ -0,0 +1,19 @@
+
+window.addEventListener('load', function () {
+    const temperature = parseFloat(document.querySelector("#current-temp").textContent);
+    const windSpeed = parseFloat(document.querySelector("#windspeed").textContent);
+    console.log("temp:" + temperature);
+    console.log("wind:" + windSpeed);
+
+    function culculateWindChill (temperature, windSpeed) {
+        return Math.round(35.74 + 0.6215 * temperature - 35.75 * (windSpeed ** 0.16) + 0.4275 * temperature * (windSpeed ** 0.16))
+    };
+    console.log(culculateWindChill(temperature, windSpeed));
+    if (temperature <= 50 && windSpeed >= 3) {
+        document.querySelector("#windchill").textContent = `${culculateWindChill(temperature, windSpeed)} °F`;
+        
+    } 
+    else {
+        document.querySelector("#windchill").textContent = "N/A";
+    }
+});
